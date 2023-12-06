3c32023db24916acb5ccafa421cbc20f59e7d4ee
kalandar1305 <kalandar1305@gmail.com>
2023-12-06 14:54:56 +0530

    message1

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import "../src/Styles/modern-normalise.css"
+import Header from './Components/Header/Header'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <Header />
+      </div>
+    
+    </>
+  )
+}
+
+export default App
---
+++ portfolio/src/App.jsx
@@ -0,0 +1,10 @@
+import Nav from "./Components/nav"
+const App = () => {
+  return (
+    <div className="bg-slate-900">
+      <Nav/>
+    </div> 
+  )
+}
+
+export default App
