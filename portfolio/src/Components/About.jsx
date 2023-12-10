00e09a2df53434497574dc2455d4dc2ca01b8912
kalandar1305 <kalandar1305@gmail.com>
2023-12-10 13:26:44 +0530

    banner

--- portfolio/src/Components/About.jsx
+++ portfolio/src/Components/About.jsx
@@ -2,43 +2,67 @@ import Aos from "aos";
 import { useEffect } from "react";
 
 const About = () => {
-  useEffect( ()=>{
+  useEffect(() => {
     Aos.init({
-
       duration: 1000,
     });
-  },[])
+  }, []);
   return (
-
-    <div id="About" className="lg:px-56 px-10 lg:py-0 py-20 text-center gap-5 lg-text-start flex lg:flex-row flex-col justify-between lg:gap-28 items-center">
-      <img data-aos="fade-down"
+    <div
+      id="About"
+      className="lg:px-56 px-10 lg:py-0 py-20 text-center gap-5 lg-text-start flex lg:flex-row flex-col justify-between lg:gap-28 items-center"
+    >
+      <img
+        data-aos="fade-down"
         src=""
         className="rounded w-56 h-56 border-2 p-1  img_glow"
         alt="profile"
       ></img>
-      <div   className="h-full lg:py-40 flex flex-col justify-center lg:items-start items-center text-white">
-        <h2 data-aos="fade-right" className="text-[52px] font-semibold mb-8 leading-normal text-fuchsia-500 uppercase">
+      <div className="h-full lg:py-40 flex flex-col justify-center  items-center text-white">
+        <h2
+          data-aos="fade-right"
+          className="text-[52px] font-semibold mb-8 leading-normal text-fuchsia-500 uppercase "
+        >
           About me
         </h2>
         <p data-aos="fade-left">
-          asicbabcla aiocacaicail biacabicab c aican Lorem ncsdjkchekch ecu ue
-          ceuc euebcueybcueyce u ecey ceoc e cceuc eucneoucey
+          I recently completed my Bachelor of Engineering in{" "}
+          <span className="text-fuchsia-800 uppercase text-[18px]">
+            Information Science and Engineering
+          </span>{" "}
+          in 2023 at NMAM Institute of Technology, Nitte, with a notable CGPA of
+          8.87.
         </p>
-        <div className="flex mt-8 gap-2">
-          <div className="flex items-center justify-center">
-            <div className="flex space-x-2">
-              <a href="https://bit.ly/Kalaandar_resume" target="_blank" rel="noreferrer"><button className="neno-button shadow-xl hover:shadow-fuchsia-800/50 text-white border-2 hover:bg-fuchsia-800 border-fuchsia-800 rounded-lg py-2 px-8 uppercase relative overflow-hidden  ">
+        <p data-aos="fade-left">Currently, I am actively learning the{" "}
+        <span className="text-fuchsia-800 uppercase text-[18px]">
+          MERN stack
+        </span>{" "}
+        and enthusiastically{" "}
+        <span className="text-fuchsia-800 uppercase text-[18px]">
+          open to opportunities
+        </span>{" "}
+        in the software field.{" "}</p>
+       
+        <div className="flex justify-center space-x-4 mt-8"  data-aos="fade-right">
+          <a
+            href="https://bit.ly/Kalaandar_resume"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="neno-button shadow-xl hover:shadow-fuchsia-800/50 text-white border-2 hover:bg-fuchsia-800 border-fuchsia-800 rounded-lg py-2 px-8 uppercase relative overflow-hidden">
               Resume
-            </button></a>
-              
-            </div>
-            <div className="flex space-x-2">
-            <a href="https://bit.ly/Kalandar-certificates" target="_blank" rel="noreferrer"><button className="neno-button shadow-xl hover:shadow-fuchsia-800/50 text-white border-2 hover:bg-fuchsia-800 border-fuchsia-800 rounded-lg py-2 px-8 uppercase relative overflow-hidden  ">
-            Certificates
-          </button></a>
-            
-          </div>
-          </div>
+            </button>
+          </a>
+
+          <a
+            href="https://bit.ly/Kalandar-certificates"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="neno-button shadow-xl hover:shadow-fuchsia-800/50 text-white border-2 hover:bg-fuchsia-800 border-fuchsia-800 rounded-lg py-2 px-8 uppercase relative overflow-hidden">
+              Certificates
+            </button>
+          </a>
         </div>
       </div>
     </div>
--- portfolio/src/Components/Banner.jsx
+++ portfolio/src/Components/Banner.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import "aos/dist/aos.css";
@@ -5,32 +6,50 @@ import { useEffect } from "react";
 import Aos from "aos";
 
 const Banner = () => {
-  useEffect( ()=>{
+  useEffect(() => {
     Aos.init({
-
       duration: 1000,
     });
-  },[])
-  
+  }, []);
 
   return (
-    <div  className="lg:px-56 px-10 lg:py-0 py-20 text-center gap-5 lg-text-start flex lg:flex-row flex-col-reverse justify-between lg:gap-28 items-center">
-      <div className="h-full lg:py-40 flex flex-col justify-center lg:items-start items-center text-white">
-        <h2 data-aos="fade-right"   className="text-[52px] font-semibold mb-8 leading-normal uppercase">
-          Welcome to <span className="text-fuchsia-500">My Website</span>{" "}
-        </h2>
-        <p data-aos="fade-left"  >You can also use variant modifiers to target media queries like responsive breakpoints, dark mode, prefers-reduced-motion, and more. For example, use md:shadow-lg to apply the shadow-lg utility at only medium screen sizes and above.</p>
-        <div className="flex mt-8 gap-2">
+    <div className="lg:px-56 px-10 lg:py-0 py-20 text-center gap-5 lg-text-start flex lg:flex-row flex-col-reverse justify-between lg:gap-28 items-center">
+      <div className="h-full lg:py-40 flex flex-col  justify-center lg:items-start items-center text-white ">
+        <div className="flex flex-col ">
+          <h2
+            data-aos="fade-right"
+            className="text-[48px] font-semibold mb-8 leading-normal uppercase"
+          >
+            Welcome to <span className="text-fuchsia-500">My Website</span>{" "}
+          </h2>
+
+          <p
+            data-aos="fade-right"
+            className=" text-2xl font-bold mb-2 leading-normal"
+          >
+            Hello, I'm Kalandar
+          </p>
+          <p
+            data-aos="fade-right"
+            className=" text-2xl font-bold mb-8 leading-normal"
+          >
+            Software Developer
+          </p>
+          <div className="flex justify-center  gap-2">
           <div className="flex items-center justify-center">
             <div className="flex space-x-2">
               <a
-                href="https://github.com/Kalandar1305" target="_blank" rel="noreferrer"
+                href="https://github.com/Kalandar1305"
+                target="_blank"
+                rel="noreferrer"
                 className="text-fuchsia-600 hover:text-fuchsia-500 rounded-full glow p-2"
               >
                 <FaGithub className="text-[28px]" />
               </a>
-              <a 
-                href="https://www.linkedin.com/in/kalandar-bbb9461b6/" target="_blank" rel="noreferrer"
+              <a
+                href="https://www.linkedin.com/in/kalandar-bbb9461b6/"
+                target="_blank"
+                rel="noreferrer"
                 className="text-fuchsia-600 hover:text-fuchsia-500 rounded-full glow p-2"
               >
                 <FaLinkedin className="text-[28px]" />
@@ -42,7 +61,9 @@ const Banner = () => {
                 <FaXTwitter className="text-[28px]" />
               </a>
               <a
-                href="https://www.instagram.com/___kalandar/" target="_blank" rel="noreferrer"
+                href="https://www.instagram.com/___kalandar/"
+                target="_blank"
+                rel="noreferrer"
                 className="text-fuchsia-600 hover:text-fuchsia-500 rounded-full glow p-2"
               >
                 <FaInstagram className="text-[28px]" />
@@ -50,8 +71,13 @@ const Banner = () => {
             </div>
           </div>
         </div>
+        </div>
+        
+
+        
       </div>
-      <img data-aos="fade-up"  
+      <img
+        data-aos="fade-up"
         src=""
         className="rounded-full w-56 h-56 border-2 p-1   img_glow"
         alt="profile"
